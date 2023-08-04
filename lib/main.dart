5c71afa66928607bebe262db5589fa6cce5278a2
AprilCat <1004887615@qq.com>
2023-08-04 17:10:57 +0800

    ex3:图片

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(
@@ -13,22 +23,19 @@ void main() {
   );
 }
 
+const url = 'https://bit.ly/3qYOtDm';
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-         });
-        return null;
-      },
-      [controller],
-    );
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+
+    final snapshot = useFuture(future);
 
     return Scaffold(
       appBar: AppBar(
@@ -36,11 +43,8 @@ class HomePage extends HookWidget {
       ),
       body: Column(
         children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ],
+          snapshot.data,
+        ].compactMap().toList(),
       ),
     );
   }

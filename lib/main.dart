ce0c9a7a9c4b307057653e110d257e0f3234281a
AprilCat <1004887615@qq.com>
2023-08-04 21:34:22 +0800

    ex7:useReducer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -19,43 +18,120 @@ void main() {
 const url =
     'https://cdn.jsdelivr.net/gh/q1uyj/BlogImage/img/202308042005143.png';
 
+enum Action {
+  rotateLeft,
+  rotateRight,
+  moreVisible,
+  lessVisible,
+}
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+
+  const State({
+    required this.rotationDeg,
+    required this.alpha,
+  });
+
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateRight() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg + 10.0,
+      );
+
+  State rotateLeft() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg - 10.0,
+      );
+
+  State increaseAlpha() => State(
+        alpha: min(alpha + 0.1, 1.0),
+        rotationDeg: rotationDeg,
+      );
+
+  State decreaseAlpha() => State(
+        alpha: max(alpha - 0.1, 0.0),
+        rotationDeg: rotationDeg,
+      );
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+    case Action.rotateRight:
+      return oldState.rotateRight();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case null:
+      return oldState;
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(onListen: () {
-      controller.sink.add(0.0);
-    });
-
+    final store = useReducer<State, Action?>(
+      reducer,
+      initialState: const State.zero(),
+      initialAction: null,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: StreamBuilder<double>(
-          stream: controller.stream,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const CircularProgressIndicator();
-            } else {
-              final rotation = snapshot.data ?? 0.0;
-              return GestureDetector(
-                onTap: () {
-                  controller.sink.add(rotation + 10.0);
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.rotateLeft);
                 },
-                child: RotationTransition(
-                  turns: AlwaysStoppedAnimation(rotation / 360.0),
-                  child: Center(
-                    child: Image.network(
-                      url,
-                      height: 300,
-                    ),
-                  ),
+                child: const Text('Rotate Left'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.rotateRight);
+                },
+                child: const Text('Rotate Right'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.lessVisible);
+                },
+                child: const Text('Less Visible'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.moreVisible);
+                },
+                child: const Text('More Visible'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+                turns: AlwaysStoppedAnimation(
+                  store.state.rotationDeg / 360.0,
                 ),
-              );
-            }
-          }),
+                child: Image.network(url)),
+          ),
+        ],
+      ),
     );
   }
 }

2bd2b067a853ed35a7424500de008c30095c047d
AprilCat <1004887615@qq.com>
2023-08-04 15:22:19 +0800

    ex2:文本框

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,34 @@ void main() {
   );
 }
 
-/// 使用 Stream.periodic 创建周期性 Stream
-/// 接收两个参数:
-/// 1. 每个周期的时间间隔
-/// 2. 回调函数, 用于生成流中的每个元素
-/// 没有使用这个函数的参数所以用_表示
-Stream<String> getTime() => Stream.periodic(
-      const Duration(seconds: 1),
-      (_) => DateTime.now().toIso8601String(),
-    );
-
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(
+      () {
+        controller.addListener(() {
+          text.value = controller.text;
+         });
+        return null;
+      },
+      [controller],
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? 'Home page'),
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: controller,
+          ),
+          Text('You typed ${text.value}'),
+        ],
       ),
     );
   }

73abfd1b51916d3fb339a3be46be8287185eae5b
AprilCat <1004887615@qq.com>
2023-08-04 20:14:33 +0800

    ex5:动画效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -16,22 +15,20 @@ void main() {
   );
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(
-      const Duration(seconds: 1),
-      (v) => from - v,
-    ).takeWhile((value) => value >= 0).listen((value) {
-      this.value = value;
-    });
-  }
+const url =
+    'https://cdn.jsdelivr.net/gh/q1uyj/BlogImage/img/202308042005143.png';
+const imageHeight = 300.0;
 
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
+extension Normalize on num {
+  num normalized(
+    num selfRangeMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0.0,
+    num normalizedRangeMax = 1.0,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) *
+          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
+      normalizedRangeMin;
 }
 
 class HomePage extends HookWidget {
@@ -39,14 +36,60 @@ class HomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized(() => CountDown(from: 20));
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacipy = max(imageHeight - controller.offset, 0.0);
+        final normalized = newOpacipy.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Text(
-        notifier.value.toString(),
+      body: Column(
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Person ${index + 1}'),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }

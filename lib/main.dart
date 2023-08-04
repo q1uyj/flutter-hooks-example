dc5448d682dadb2a3e69f2ce76d03a4de65a1ae2
AprilCat <1004887615@qq.com>
2023-08-04 21:55:56 +0800

    ex8: useAppLifecycleState

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -16,122 +14,34 @@ void main() {
 }
 
 const url =
-    'https://cdn.jsdelivr.net/gh/q1uyj/BlogImage/img/202308042005143.png';
-
-enum Action {
-  rotateLeft,
-  rotateRight,
-  moreVisible,
-  lessVisible,
-}
-
-@immutable
-class State {
-  final double rotationDeg;
-  final double alpha;
-
-  const State({
-    required this.rotationDeg,
-    required this.alpha,
-  });
-
-  const State.zero()
-      : rotationDeg = 0.0,
-        alpha = 1.0;
-
-  State rotateRight() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg + 10.0,
-      );
-
-  State rotateLeft() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg - 10.0,
-      );
-
-  State increaseAlpha() => State(
-        alpha: min(alpha + 0.1, 1.0),
-        rotationDeg: rotationDeg,
-      );
-
-  State decreaseAlpha() => State(
-        alpha: max(alpha - 0.1, 0.0),
-        rotationDeg: rotationDeg,
-      );
-}
-
-State reducer(State oldState, Action? action) {
-  switch (action) {
-    case Action.rotateLeft:
-      return oldState.rotateLeft();
-    case Action.rotateRight:
-      return oldState.rotateRight();
-    case Action.lessVisible:
-      return oldState.decreaseAlpha();
-    case Action.moreVisible:
-      return oldState.increaseAlpha();
-    case null:
-      return oldState;
-  }
-}
+    'https://cdn.jsdelivr.net/gh/q1uyj/BlogImage/img/202308042149153.png';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final store = useReducer<State, Action?>(
-      reducer,
-      initialState: const State.zero(),
-      initialAction: null,
-    );
+    final state = useAppLifecycleState();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  store.dispatch(Action.rotateLeft);
-                },
-                child: const Text('Rotate Left'),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: Opacity(
+          opacity: state == AppLifecycleState.resumed ? 1.0 : 0.0,
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.black.withAlpha(100),
+                    spreadRadius: 10,
+                  )
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  store.dispatch(Action.rotateRight);
-                },
-                child: const Text('Rotate Right'),
-              ),
-              TextButton(
-                onPressed: () {
-                  store.dispatch(Action.lessVisible);
-                },
-                child: const Text('Less Visible'),
-              ),
-              TextButton(
-                onPressed: () {
-                  store.dispatch(Action.moreVisible);
-                },
-                child: const Text('More Visible'),
-              ),
-            ],
+              child: Image.network(url),
+            ),
           ),
-          const SizedBox(
-            height: 100,
-          ),
-          Opacity(
-            opacity: store.state.alpha,
-            child: RotationTransition(
-                turns: AlwaysStoppedAnimation(
-                  store.state.rotationDeg / 360.0,
-                ),
-                child: Image.network(url)),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
